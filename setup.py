dc8c83ccd961595da2c43012cf24d1fbcc21b1e5
divakar patil <divakarpatil35@gmail.com>
2021-06-27 12:25:18 +0530

    Setting up base structure for the project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='snakesandladders',
+    version='V1.0',
+    packages=['tests', 'snakesandladders'],
+    url='',
+    license='',
+    author='Divakar Patil',
+    author_email='divakarpatil35@gmail.com',
+    description=''
+)
